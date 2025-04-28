2747bb2a9de6b5c118305a3a84423a03016c5fe6
Abinaya Sudhir <abinaya@atomicits.com>
2025-04-28 14:11:49 +0530

    functions

---
+++ definition.rs
@@ -0,0 +1,7 @@
+fn main() {
+    call_me(9395550113);
+}
+
+fn call_me(num: i64) {
+    println!("Ring! Call number {}", num);
+}
---
+++ return_keyword.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let i_woke_up_late = true;
+    morning_routine(i_woke_up_late);
+}
+
+fn morning_routine(i_am_late: bool) {
+    println!("This morning, I...");
+    if i_am_late {
+        go_to_work();
+        return;
+    }
+    exercise();
+    eat_breakfast();
+    make_coffee();
+    go_to_work();
+}
+
+fn exercise() {
+    println!("I went to the gym.");
+}
+
+fn eat_breakfast() {
+    println!("I had a healthy breakfast!");
+}
+
+fn make_coffee() {
+    println!("I made myself coffee. Now that I'm ready...");
+}
+
+fn go_to_work() {
+    println!("I went straight to work!");
+}
