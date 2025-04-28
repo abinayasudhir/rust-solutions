f12b62035e68fd3166db4683b28508cdeb1da473
Abinaya Sudhir <abinaya@atomicits.com>
2025-04-28 16:08:06 +0530

    some more exercises

---
+++ option/if_let.rs
@@ -0,0 +1,25 @@
+struct User {
+    id: u32,
+    name: String,
+}
+
+fn get_user_name(id: u32) -> Option<String> {
+    let database = [
+        User {id: 1, name: String::from("Alice")},
+        User {id: 2, name: String::from("Bob")},
+        User {id: 3, name: String::from("Cindy")}
+    ];
+    for user in database {
+        if user.id == id {
+            return Some(user.name)
+        }
+    }
+    None
+}
+
+fn main() {
+    let user_id = 3;
+    if let Some(name) = get_user_name(user_id) {
+        println!("User's name: {name}");
+    }
+}
---
+++ result/return_result.rs
@@ -0,0 +1,15 @@
+fn greet(name: &str) -> Result<(), String> {
+    if name.len() > 0 {
+        println!("Hello {name}!");
+        Ok(())
+    } else {
+        Err("Empty name provided".to_string())
+    }
+}
+
+fn main() {
+    let name = "Tom";
+    if let Err(e) = greet(name) {
+        println!("Error: {e}");
+    }
+}
